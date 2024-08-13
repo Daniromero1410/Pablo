36c6e3d6b0032bce69d6c6ee796f5c1a1e10906a
Daniel Romero ;D <130934263+Daniromero1410@users.noreply.github.com>
2024-08-13 11:54:39 -0500

    Solucion Error

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.http.response import JsonResponse
-from django.shortcuts import render,
+from django.shortcuts import render
 from .models import Ingenieria
 # Create your views here.
 def index(request):
@@ -9,6 +9,3 @@ def list_ingenierias(_request):
     ingenierias=list(Ingenieria.objects.values())
     data={'Ingenieria':ingenierias}
     return JsonResponse(data)
-
-def detalle_proyecto(request, ingenieria_id):
-    ingenieria= get_object_or_404()
